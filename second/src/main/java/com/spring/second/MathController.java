724c4baf9815e58e128bcdebebe0219d9ee34176
Rubens Soares <rubens.soares@liferay.com>
2024-05-21 14:03:44 -0300

    Delete the Greeting and GreetingController classes and Add MathController

---
+++ second/src/main/java/com/spring/second/MathController.java
@@ -0,0 +1,16 @@
+package com.spring.second;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double multiplication(@PathVariable(value = "numberOne") String numberOne,
+                                 @PathVariable(value = "numberTwo") String numberTwo) {
+        return 1D;
+    }
+}
