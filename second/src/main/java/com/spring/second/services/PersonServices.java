637b8bcb4e0b947c395aa0512d4ea5ca5233cdec
Rubens Soares <rubens.soares@liferay.com>
2024-06-11 10:16:56 -0300

    Add method findAll in PersonServices class

--- second/src/main/java/com/spring/second/services/PersonServices.java
+++ second/src/main/java/com/spring/second/services/PersonServices.java
@@ -4,6 +4,9 @@ import com.spring.second.model.Person;
 import org.apache.juli.logging.Log;
 import org.springframework.stereotype.Service;
 
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +15,17 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+
+        logger.info("Find all people");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
 
     public Person findById(String id) {
 
@@ -22,6 +36,17 @@ public class PersonServices {
         person.setFirstname("Leandro");
         person.setLastName("Costa");
         person.setAddress("Uberlandia - Minas Gerais - Brasil");
+        person.setGender("Male");
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstname("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Some address in Brazil " + i);
         person.setGender("Male");
         return person;
     }
