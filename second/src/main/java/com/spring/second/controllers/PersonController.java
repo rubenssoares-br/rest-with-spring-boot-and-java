aaf260ed6ba29ffe322a00a2ecc9d7488001660c
Rubens Soares <rubens.soares@liferay.com>
2024-06-15 20:56:30 -0300

    Add create method of type POST on PersonController class

--- second/src/main/java/com/spring/second/controllers/PersonController.java
+++ second/src/main/java/com/spring/second/controllers/PersonController.java
@@ -4,10 +4,7 @@ import com.spring.second.model.Person;
 import com.spring.second.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,11 @@ public class PersonController {
     public List<Person> findAll() {
         return service.findAll();
     }
+
+    @RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
 
 }
