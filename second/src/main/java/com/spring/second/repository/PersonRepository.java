4e2bd2a36b51ebb11a90d1721cd11398a18343eb
Rubens Soares <rubens.soares@liferay.com>
2024-07-18 09:38:08 -0300

    Add Repository annotation to PersonRepository interface

--- second/src/main/java/com/spring/second/repository/PersonRepository.java
+++ second/src/main/java/com/spring/second/repository/PersonRepository.java
@@ -2,6 +2,8 @@ package com.spring.second.repository;
 
 import com.spring.second.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
