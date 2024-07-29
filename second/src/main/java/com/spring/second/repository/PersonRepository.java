0c8408b0b2895e00839ba301da1328e00aac9069
Rubens Soares <rubens.soares@liferay.com>
2024-07-29 15:39:31 -0300

    Add RepositoryDefinition and Component from springframework package

--- second/src/main/java/com/spring/second/repository/PersonRepository.java
+++ second/src/main/java/com/spring/second/repository/PersonRepository.java
@@ -2,6 +2,8 @@ package com.spring.second.repository;
 
 import com.spring.second.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
