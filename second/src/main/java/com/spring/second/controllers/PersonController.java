9dc035844e434472ae19a2ea372c46b1bbcf45f9
Rubens Soares <rubens.soares@liferay.com>
2024-07-30 16:32:45 -0300

    Change the type of the http methods of Controler from Person to PersonVO

--- second/src/main/java/com/spring/second/controllers/PersonController.java
+++ second/src/main/java/com/spring/second/controllers/PersonController.java
@@ -1,7 +1,7 @@
 package com.spring.second.controllers;
 
-import com.spring.second.model.Person;
 import com.spring.second.services.PersonServices;
+import com.spring.second.vo.v1.PersonVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,22 +17,22 @@ public class PersonController {
     private PersonServices service;
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable(value = "id") Long id) throws Exception {
+    public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
         return service.findById(id);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll() {
+    public List<PersonVO> findAll() {
         return service.findAll();
     }
 
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(@RequestBody Person person) {
+    public PersonVO create(@RequestBody PersonVO person) {
         return service.create(person);
     }
 
     @PutMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(@RequestBody Person person) {
+    public PersonVO update(@RequestBody PersonVO person) {
         return service.update(person);
     }
 
