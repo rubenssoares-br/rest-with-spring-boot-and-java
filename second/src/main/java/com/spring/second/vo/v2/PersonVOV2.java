85f6c4a7177a99142e915d31d196e8f64b1cf1e4
Rubens Soares <rubens.soares@liferay.com>
2024-08-10 16:44:07 -0300

    Add PersonVOV2 class

---
+++ second/src/main/java/com/spring/second/vo/v2/PersonVOV2.java
@@ -0,0 +1,84 @@
+package com.spring.second.vo.v2;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+
+public class PersonVOV2 implements Serializable {
+
+    private Long id;
+
+    private String firstname;
+
+    private String lastName;
+
+    private String address;
+
+    private String gender;
+
+    private Date birthDay;
+
+    public PersonVOV2() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonVOV2 that = (PersonVOV2) o;
+        return Objects.equals(id, that.id) && Objects.equals(firstname, that.firstname) && Objects.equals(lastName, that.lastName) && Objects.equals(address, that.address) && Objects.equals(gender, that.gender) && Objects.equals(birthDay, that.birthDay);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstname, lastName, address, gender, birthDay);
+    }
+}
