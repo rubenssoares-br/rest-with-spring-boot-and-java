ec0272cf49326b25e2a9e69fae47cdbb99d53abc
Rubens Soares <rubens.soares@liferay.com>
2024-08-10 16:32:26 -0300

    Add a createV2 method to PersonController

--- second/src/main/java/com/spring/second/controllers/PersonController.java
+++ second/src/main/java/com/spring/second/controllers/PersonController.java
@@ -2,6 +2,7 @@ package com.spring.second.controllers;
 
 import com.spring.second.services.PersonServices;
 import com.spring.second.vo.v1.PersonVO;
+import com.spring.second.vo.v2.PersonVOV2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +30,11 @@ public class PersonController {
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public PersonVO create(@RequestBody PersonVO person) {
         return service.create(person);
+    }
+
+    @PostMapping(value = "/v2", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
+        return service.createV2(person);
     }
 
     @PutMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
