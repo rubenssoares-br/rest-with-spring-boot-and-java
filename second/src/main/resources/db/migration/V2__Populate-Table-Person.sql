288f99f17d0195b087a9e2a21f087c749f7769e9
Rubens Soares <rubens.soares@liferay.com>
2024-08-19 15:51:55 -0300

    Add V2__Populate-Table-Person.sql

---
+++ second/src/main/resources/db/migration/V2__Populate-Table-Person.sql
@@ -0,0 +1,13 @@
+--
+-- Dumping data for table `person`
+--
+
+LOCK TABLES `person` WRITE;
+
+INSERT INTO `person` VALUES (1,'Ayrton','Senna','Sao Paulo','Male'),
+                               (2,'Joao','Vitor','Pernambuco - Recife','Male'),
+                               (4,'Luisa','Sonza','Rio Grande do Norte','Female'),
+                               (6,'Renan','Cavalcante','Rio Grande do Sul','Male'),
+                               (7,'Pedro','Tesla','Diamond Bar','Male');
+
+UNLOCK TABLES;
