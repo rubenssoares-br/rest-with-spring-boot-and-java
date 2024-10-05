ac262e7c9bdc53bc824fc3e4473807eb448b601e
Rubens Soares <rubens.soares@liferay.com>
2024-10-05 18:30:13 -0300

    Content negotiation via header parameter

--- second/src/main/java/com/spring/second/config/WebConfig.java
+++ second/src/main/java/com/spring/second/config/WebConfig.java
@@ -9,12 +9,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorParameter(true)
+
+        //Via Query Param. http://localhost:8080/api/person/v1?mediaType=xml
+       /* configurer.favorParameter(true)
                 .parameterName("mediaType")
                 .ignoreAcceptHeader(true)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML);
+      */
+
+          configurer.favorParameter(false)
+          .ignoreAcceptHeader(false)
+          .useRegisteredExtensionsOnly(false)
+          .defaultContentType(MediaType.APPLICATION_JSON)
+          .mediaType("json", MediaType.APPLICATION_JSON)
+          .mediaType("xml", MediaType.APPLICATION_XML);
     }
 }
