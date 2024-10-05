5c159fc9d30fda0d66c0db5bcbee232a46bfcd81
Rubens Soares <rubens.soares@liferay.com>
2024-10-05 18:32:28 -0300

    Refactor PersonServices to accept HATEOS in findAll and findById methods

--- second/src/main/java/com/spring/second/services/PersonServices.java
+++ second/src/main/java/com/spring/second/services/PersonServices.java
@@ -1,5 +1,6 @@
 package com.spring.second.services;
 
+import com.spring.second.controllers.PersonController;
 import com.spring.second.exceptions.ResourceNotFoundException;
 import com.spring.second.mapper.ModelMapperUtils;
 import com.spring.second.mapper.custom.PersonMapper;
@@ -8,11 +9,14 @@ import com.spring.second.repository.PersonRepository;
 import com.spring.second.vo.v1.PersonVO;
 import com.spring.second.vo.v2.PersonVOV2;
 import org.modelmapper.ModelMapper;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonServices {
@@ -22,22 +26,43 @@ public class PersonServices {
     @Autowired
     PersonRepository repository;
 
-    @Autowired
-    PersonMapper mapper;
+    private static ModelMapper mapper = new ModelMapper();
 
-    public List<PersonVO> findAll() {
-
-        return ModelMapperUtils.parseListObjects(repository.findAll(), PersonVO.class);
+    static {
+        mapper.createTypeMap(
+                        Person.class,
+                        PersonVO.class)
+                .addMapping(Person::getId, PersonVO::setKey);
     }
 
-    public PersonVO findById(Long id) {
+
+    public List<PersonVO> findAll() {
+        logger.info("Finding all people!");
+
+        return repository.findAll().stream()
+                .map(person -> {
+                    PersonVO personVO = mapper.map(person, PersonVO.class);
+                    try {
+                        personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    return personVO;
+                })
+                .collect(Collectors.toList());
+    }
+
+
+    public PersonVO findById(Long id) throws Exception {
 
         logger.info("Finding one person!");
 
         var entity = repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 
-        return ModelMapperUtils.parseObject(entity, PersonVO.class);
+        PersonVO vo = ModelMapperUtils.parseObject(entity, PersonVO.class);
+        vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+        return vo;
     }
 
     public PersonVO create(PersonVO person) {
@@ -52,23 +77,12 @@ public class PersonServices {
 
     }
 
-    public PersonVOV2 createV2(PersonVOV2 person) {
-
-        logger.info("Creating one person with V2!");
-
-        var entity = mapper.convertVOtoEntity(person);
-
-        var vo = mapper.convertEntityToVo(repository.save(entity));
-
-        return vo;
-
-    }
 
     public PersonVO update(PersonVO person) {
 
         logger.info("Updating one person!");
 
-      var entity = repository.findById(person.getId())
+      var entity = repository.findById(person.getKey())
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 
         entity.setFirstname(person.getFirstname());
